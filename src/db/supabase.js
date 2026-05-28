c7ee6a8bfcf52e26e4420c6f14296c6b6dcc120c
tilanayaje <3p1f123@gmail.com>
2026-05-28 17:06:56 -0400

    use env variables for supabase keys

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://haiendrqdnnnyqyruvxl.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_SCGT3f744EZ1m6iQBOvr-A_O8kZ1gkS';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
